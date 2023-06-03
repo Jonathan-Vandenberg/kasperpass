b8528cb400d4c03cb1a8fcae6c65aa2eb1ad3e8c
jon-vdb <jonvdberg8@gmail.com>
2023-06-04 01:51:10 +0700

    added service workers

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
-export default function Home() {
-  return <main className="max-h-screen">Home page</main>;
+import { Home } from "lucide-react";
+import Script from "next/script";
+
+export const metadata = {
+  title: "Next PWA",
+  description: "Next PWA example using next-pwa and app directory",
+};
+
+export default function Page() {
+  return (
+    <main title="App" className="max-h-screen">
+      <Script src="/register-sw.js" async />
+      <Home />
+    </main>
+  );
 }

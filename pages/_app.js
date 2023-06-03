408df4fd7fbafe39383acdfd695f5503230f9d5c
jon-vdb <jonvdberg8@gmail.com>
2023-06-04 03:04:17 +0700

    Your Decentralised Connection

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,12 @@ export default function App({ Component, pageProps }) {
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="viewport"
+          content="width=device-width; initial-scale=1; viewport-fit=cover"
+        />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="theme-color" content="#ffffff" />
       </Head>
       <div className={inter.className}>

efcb38680281e1a19f3bf4f114c15b3da2de2e11
jon-vdb <jonvdberg8@gmail.com>
2023-06-12 22:29:53 +0700

    camera in ID page

--- app/identification/page.tsx
+++ app/identification/page.tsx
@@ -4,14 +4,13 @@ import React, { useState } from "react";
 
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import Camera from "@/components/ui/camera";
+import Container from "@/components/ui/container";
 
 export default function Page() {
   return (
-    <div className="">
-      <h2>Identification Profile Page</h2>
-      <Button>
-        <Link href="/">Home</Link>
-      </Button>
-    </div>
+    <Container>
+      <Camera />
+    </Container>
   );
 }
--- components/ui/camera.tsx
+++ components/ui/camera.tsx
@@ -1,55 +1,59 @@
-// "use client";
+"use client";
 
-// import React, { useState } from "react";
+import React, { useState } from "react";
 
-// import Link from "next/link";
-// import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
-// export default function Camera() {
-//   const [source, setSource] = useState("");
+export default function Camera() {
+  const [source, setSource] = useState("");
 
-//   const hiddenFileInput = React.useRef(null);
+  const hiddenFileInput = React.useRef(null);
 
-//   const handleCapture = (target: EventTarget & HTMLInputElement) => {
-//     if (target.files) {
-//       if (target.files.length !== 0) {
-//         const file = target.files[0];
-//         const newUrl = URL.createObjectURL(file);
-//         setSource(newUrl);
-//         console.log("url: " + newUrl);
-//         console.log("sourceUrl: " + source);
-//       }
-//     }
-//   };
+  const handleCapture = (target: EventTarget & HTMLInputElement) => {
+    if (target.files) {
+      if (target.files.length !== 0) {
+        const file = target.files[0];
+        const newUrl = URL.createObjectURL(file);
+        setSource(newUrl);
+        console.log("url: " + newUrl);
+        console.log("sourceUrl: " + source);
+      }
+    }
+  };
 
-//   return (
-//     <div>
-//       <Button>
-//         <Link href="/">Home</Link>
-//       </Button>
-//       <div className="h-full text-center">
-//         <div>
-//           <div className="w-full">
-//             <h5>Capture your image</h5>
-//             {source && (
-//               <div className="flex justify-center w-36 h-36 bg-gray-400">
-//                 <img src={source} alt={"snap"} className="h-36 w-36" />
-//               </div>
-//             )}
-//             <input
-//               accept="image/*"
-//               className=""
-//               type="file"
-//               capture="environment"
-//               ref={hiddenFileInput}
-//               onChange={(e) => handleCapture(e.target)}
-//             />
-//             <label>
-//               <button aria-label="upload picture">Camera Icon here</button>
-//             </label>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
+  return (
+    <div className="w-full flex flex-col justify-center items-center pt-12">
+      {source && (
+        <div className="flex justify-center bg-gray-400 w-[80vw] rounded-full">
+          {source != "" ? (
+            <Image src={source} alt={"snap"} width={720} height={720} />
+          ) : (
+            <div className="w-72 h-72 bg-green flex items-center justify-center">
+              HI
+            </div>
+          )}
+        </div>
+      )}
+      <div className="hidden">
+        <input
+          accept="image/*"
+          size={60}
+          className=""
+          id="file-upload"
+          type="file"
+          capture="environment"
+          ref={hiddenFileInput}
+          onChange={(e) => handleCapture(e.target)}
+        />
+      </div>
+      <label
+        className="mt-36 custom-file-upload px-7 py-4 text-thin rounded-full border-2 border-gray-200 bg-cyan-800 text-white"
+        htmlFor="file-upload"
+      >
+        Upload picture
+      </label>
+    </div>
+  );
+}

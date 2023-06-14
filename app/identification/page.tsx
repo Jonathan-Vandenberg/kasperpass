eede12caa6879bc519ee9b8d72b2b8340923fe84
jon-vdb <jonvdberg8@gmail.com>
2023-06-14 23:01:08 +0700

    change page with data

--- app/identification/page.tsx
+++ app/identification/page.tsx
@@ -1,35 +1,28 @@
 "use client";
-
 import QrScanner from "@/components/ui/QrScanner";
 import Container from "@/components/ui/container";
 import MediaDevices from "@/components/ui/MediaDevices";
 import Result from "@zxing/library/esm/core/Result";
-
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 import GenerateQRCode from "../utilities/generateQRCode";
 import CameraComponent from "@/components/ui/profile-selfie";
 import { captureAndSendImage } from "api/profile-image";
+import Link from "next/link";
 
 export default function Page() {
+  const router = useRouter();
+
   const [data, setData] = useState<Result>();
-  const [decode, setDecode] = useState({
-    name: "",
-    surname: "",
-    ID: "",
-    support: "",
-    address: "",
-    birthCertificate: "",
-    issuer: "",
-    issueDate: "",
-    expirationDate: "",
-    nationality: "",
-    mother: "",
-    father: "",
-  });
+  const [decode, setDecode] = useState("");
 
   const handleCapture = (imageData: any) => {
     captureAndSendImage();
     console.log(imageData);
+  };
+
+  const navigateToPersonalId = () => {
+    router.push(decode);
   };
 
   return (
@@ -37,34 +30,25 @@ export default function Page() {
       <QrScanner
         onResult={(result) => {
           setData(result);
+          setTimeout(() => {
+            navigateToPersonalId();
+          }, 1000);
         }}
         onDecode={(decode) => {
-          const userInfo = JSON.parse(decode);
+          const userInfo = JSON.stringify(decode);
           setDecode(userInfo);
         }}
         onError={(error) => {
           console.log("onError" + error?.message);
         }}
       />
-      <Container className="max-w-screen">
-        {/* <CameraComponent onCapture={handleCapture} /> */}
-        <>
-          {/* <MediaDevices /> */}
-          <p>Name: {decode.name}</p>
-          <p>Surname: {decode.surname}</p>
-          <p>ID: {decode.ID}</p>
-          <p>Support: {decode.support}</p>
-          <p>Address: {decode.address}</p>
-          <p>Birth Certificate: {decode.birthCertificate}</p>
-          <p>Issuer: {decode.issuer}</p>
-          <p>issueDate: {decode.issueDate}</p>
-          <p>Expiration Date: {decode.expirationDate}</p>
-          <p>Nationality: {decode.nationality}</p>
-          <p>Mother: {decode.mother}</p>
-          <p>Father: {decode.father}</p>
-        </>
-      </Container>
-      {/* <GenerateQRCode /> */}
+      <>
+        <Link href="/personal-id">Go to Personal ID</Link>
+        <button onClick={navigateToPersonalId}>
+          Go to Personal ID (via function)
+        </button>
+      </>
+      ;{/* <GenerateQRCode /> */}
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -10,11 +10,13 @@ export default function Page() {
         <div className="relative w-full mx-auto pt-6">
           <span className="absolute bg-cyan-100 rounded-bl-full w-full h-[100px] -z-10 inset-0" />
         </div>
-        <div className="flex items-center justify-center h-36 w-36 shrink-0 overflow-hidden rounded-full mx-auto mt-4">
+
+        <div className="relative flex items-center justify-center h-36 w-36 shrink-0 rounded-full mx-auto mt-4 overflow-visible">
           <Image
             src={image}
             width={500}
             height={500}
+            className="z-20 rounded-full"
             alt="Picture of the author"
           />
         </div>
@@ -22,17 +24,21 @@ export default function Page() {
       <Container>
         <div className="w-full">
           <div className="flex flex-col items-center justify-center space-y-7">
-            <div>
-              <p className="text-2xl text-center w-full leading-widest font-semibold">
-                Triana Sofía González
-              </p>
-              <p className="font-serif text-slate-500 text-sm text-center w-full">
-                0xa0b668fbeC004c410987687B53Bd6Bb7809E4Bb4
-              </p>
-              {/* <img
-                src="https://hatscripts.github.io/circle-flags/flags/sp.svg"
-                width="100"
-              ></img> */}
+            <div className="flex">
+              {/* <div className="pr-2">
+                <img
+                  src="https://hatscripts.github.io/circle-flags/flags/za.svg"
+                  width="40"
+                ></img>
+              </div> */}
+              <div className="flex flex-col items-center justify-center">
+                <p className="text-2xl text-start w-full leading-widest font-semibold">
+                  Triana Sofía González
+                </p>
+                <p className="font-serif text-slate-500 text-sm text-start w-full">
+                  0xa0b668fbeC004c410987687B53Bd6Bb7809E4Bb4
+                </p>
+              </div>
             </div>
           </div>
           <div className="pt-8">

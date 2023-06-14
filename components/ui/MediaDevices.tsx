f5d29d3b9d4d3838905b43c55d92947e6da0518e
jon-vdb <jonvdberg8@gmail.com>
2023-06-14 09:29:46 +0700

    scanner

--- components/ui/MediaDevices.tsx
+++ components/ui/MediaDevices.tsx
@@ -5,7 +5,8 @@ import { useMediaDevices } from "../../hooks/useMediaDevices";
 function Template() {
   const state = useMediaDevices();
 
-  return <pre>{JSON.stringify(state, null, 2)}</pre>;
+  // return <pre>{JSON.stringify(null, null, 2)}</pre>;
+  return;
 }
 
 export const ListDevices = Template.bind({});

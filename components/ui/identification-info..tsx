9a967460cccea1b2c38f6a9abfea57971c2763e8
jon-vdb <jonvdberg8@gmail.com>
2023-06-18 01:15:55 +0700

    card

--- components/ui/identification-info..tsx
+++ components/ui/identification-info..tsx
@@ -9,97 +9,103 @@ export default function IdentificationInfo({ information }: any) {
         <div className="flex items-center justify-between border border-1-gray rounded-full px-4 py-3 w-full whitespace-nowrap">
           <p>Name</p>
           {information.name ? (
-            <p>{information.name}</p>
+            <p className="font-semibold text-black">{information.name}</p>
           ) : (
-            <Skeleton className="w-3/5 h-[20px] bg-gray-100 rounded-full" />
+            <Skeleton className="w-3/5 h-[20px] bg-gray-100 rounded-full mr-6" />
           )}
         </div>
         <div className="flex items-center justify-between border border-1-gray rounded-full px-4 py-3 w-full whitespace-nowrap">
           <p>Surname</p>
           {information.surname ? (
-            <p>{information.surname}</p>
+            <p className="font-semibold text-black">{information.surname}</p>
           ) : (
-            <Skeleton className="w-3/5 h-[20px] bg-gray-100 rounded-full" />
+            <Skeleton className="w-3/5 h-[20px] bg-gray-100 rounded-full mr-6" />
           )}
         </div>
         <div className="flex items-center justify-between border border-1-gray rounded-full px-4 py-3 w-full whitespace-nowrap">
           <p>ID</p>
           {information.ID ? (
-            <p>{information.ID}</p>
+            <p className="font-semibold text-black">{information.ID}</p>
           ) : (
-            <Skeleton className="w-3/5 h-[20px] bg-gray-100 rounded-full" />
+            <Skeleton className="w-3/5 h-[20px] bg-gray-100 rounded-full mr-6" />
           )}
         </div>
         <div className="flex items-center justify-between border border-1-gray rounded-full px-4 py-3 w-full whitespace-nowrap">
           <p>Support</p>
           {information.support ? (
-            <p>{information.support}</p>
+            <p className="font-semibold text-black">{information.support}</p>
           ) : (
-            <Skeleton className="w-3/5 h-[20px] bg-gray-100 rounded-full" />
+            <Skeleton className="w-3/5 h-[20px] bg-gray-100 rounded-full mr-6" />
           )}
         </div>
         <div className="flex items-center justify-between border border-1-gray rounded-full px-4 py-3 w-full whitespace-nowrap">
           <p>Address</p>
           {information.address ? (
-            <p>{information.address}</p>
+            <p className="font-semibold text-black">{information.address}</p>
           ) : (
-            <Skeleton className="w-3/5 h-[20px] bg-gray-100 rounded-full" />
+            <Skeleton className="w-3/5 h-[20px] bg-gray-100 rounded-full mr-6" />
           )}
         </div>
         <div className="flex items-center justify-between border border-1-gray rounded-full px-4 py-3 w-full whitespace-nowrap">
           <p>Birth Date</p>
           {information.birthCertificate ? (
-            <p>{information.birthCertificate}</p>
+            <p className="font-semibold text-black">
+              {information.birthCertificate}
+            </p>
           ) : (
-            <Skeleton className="w-3/5 h-[20px] bg-gray-100 rounded-full" />
+            <Skeleton className="w-3/5 h-[20px] bg-gray-100 rounded-full mr-6" />
           )}
         </div>
         <div className="flex items-center justify-between border border-1-gray rounded-full px-4 py-3 w-full whitespace-nowrap">
           <p>Issuer</p>
           {information.issuer ? (
-            <p>{information.issuer}</p>
+            <p className="font-semibold text-black">{information.issuer}</p>
           ) : (
-            <Skeleton className="w-3/5 h-[20px] bg-gray-100 rounded-full" />
+            <Skeleton className="w-3/5 h-[20px] bg-gray-100 rounded-full mr-6" />
           )}
         </div>
         <div className="flex items-center justify-between border border-1-gray rounded-full px-4 py-3 w-full whitespace-nowrap">
           <p>Issue Date</p>
           {information.issueDate ? (
-            <p>{information.issueDate}</p>
+            <p className="font-semibold text-black">{information.issueDate}</p>
           ) : (
-            <Skeleton className="w-3/5 h-[20px] bg-gray-100 rounded-full" />
+            <Skeleton className="w-3/5 h-[20px] bg-gray-100 rounded-full mr-6" />
           )}
         </div>
         <div className="flex items-center justify-between border border-1-gray rounded-full px-4 py-3 w-full whitespace-nowrap">
           <p>Expiration</p>
           {information.expirationDate ? (
-            <p>{information.expirationDate}</p>
+            <p className="font-semibold text-black">
+              {information.expirationDate}
+            </p>
           ) : (
-            <Skeleton className="w-3/5 h-[20px] bg-gray-100 rounded-full" />
+            <Skeleton className="w-3/5 h-[20px] bg-gray-100 rounded-full mr-6" />
           )}
         </div>
         <div className="flex items-center justify-between border border-1-gray rounded-full px-4 py-3 w-full whitespace-nowrap">
           <p>Nationality</p>
           {information.nationality ? (
-            <p>{information.nationality}</p>
+            <p className="font-semibold text-black">
+              {information.nationality}
+            </p>
           ) : (
-            <Skeleton className="w-3/5 h-[20px] bg-gray-100 rounded-full" />
+            <Skeleton className="w-3/5 h-[20px] bg-gray-100 rounded-full mr-6" />
           )}
         </div>
         <div className="flex items-center justify-between border border-1-gray rounded-full px-4 py-3 w-full whitespace-nowrap">
           <p>Mother</p>
           {information.mother ? (
-            <p>{information.mother}</p>
+            <p className="font-semibold text-black">{information.mother}</p>
           ) : (
-            <Skeleton className="w-3/5 h-[20px] bg-gray-100 rounded-full" />
+            <Skeleton className="w-3/5 h-[20px] bg-gray-100 rounded-full mr-6" />
           )}
         </div>
         <div className="flex items-center justify-between border border-1-gray rounded-full px-4 py-3 w-full whitespace-nowrap">
           <p>Father</p>
           {information.father ? (
-            <p>{information.father}</p>
+            <p className="font-semibold text-black">{information.father}</p>
           ) : (
-            <Skeleton className="w-3/5 h-[20px] bg-gray-100 rounded-full" />
+            <Skeleton className="w-3/5 h-[20px] bg-gray-100 rounded-full mr-6" />
           )}
         </div>
       </div>
